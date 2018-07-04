f73d0c25903ae151822aad2ca771a8d17baa5847
github username <github email address>
2018-07-04 16:42:29 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(string)
+    if string == '' || string == nil
+      Student.all
+    else
+      Student.select do |student|
+        student.name.downcase.include?(string)
+      end
+    end
+  end
+
 end
